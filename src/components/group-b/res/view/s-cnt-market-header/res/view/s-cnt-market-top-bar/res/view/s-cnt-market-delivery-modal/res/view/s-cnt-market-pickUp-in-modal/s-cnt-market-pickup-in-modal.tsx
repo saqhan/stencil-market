cd9c42f677198309d4211bb20d995fdadca8cd1c
Wokh-Dada <a.gezikhanov@mail.ru>
2020-08-05 16:15:44 +0300

    add array and layot

--- src/components/group-b/res/view/s-cnt-market-header/res/view/s-cnt-market-top-bar/res/view/s-cnt-market-delivery-modal/res/view/s-cnt-market-pickUp-in-modal/s-cnt-market-pickup-in-modal.tsx
+++ src/components/group-b/res/view/s-cnt-market-header/res/view/s-cnt-market-top-bar/res/view/s-cnt-market-delivery-modal/res/view/s-cnt-market-pickUp-in-modal/s-cnt-market-pickup-in-modal.tsx
@@ -1,4 +1,4 @@
-import {Component, ComponentInterface, h, Prop} from '@stencil/core';
+import {Component, ComponentInterface, h, Prop, State} from '@stencil/core';
 
 @Component({
   tag: 's-cnt-market-pickup-in-modal',
@@ -12,6 +12,11 @@ export class SCntMarketPickupInModal implements ComponentInterface {
    */
   @Prop() pickUpData: any;
 
+  /**
+   * boolean значение для вывода компонента form-communication
+   */
+  @State() SroreAddress: boolean;
+
   render() {
     return (
       <div class="adress-in-city-blocks">
@@ -23,8 +28,11 @@ export class SCntMarketPickupInModal implements ComponentInterface {
           <div class="city-option-for-selected">
             {this.pickUpData.selectedCity}
             <span innerHTML={this.pickUpData.selectIcon}>
-
+            {/*вывод списка магазинов*/}
             </span>
+            <div class="selection-city-block">
+              <CityForSelected arr={this.pickUpData.cityForSelected}/>
+            </div>
           </div>
         </div>
         <div class="expandable-store-list">
@@ -44,14 +52,84 @@ export class SCntMarketPickupInModal implements ComponentInterface {
               <div class="number-of-stores">
                 {this.pickUpData.numberOfStores}
               </div>
-              <div class="list-stores-open-icon" innerHTML={this.pickUpData.selectedStoreIcon}>
+              <div class="list-stores-open-icon" innerHTML={this.pickUpData.selectedStoreIcon} onClick={() => {
+                this.opClStoreAddress();
+                // this.closeStoreAddress();
+              }}>
                 {/*иконка выбора магазина*/}
               </div>
             </div>
           </div>
+          {
+            this.SroreAddress ?
+              <SroreAddress arr={this.pickUpData.storeAddress}/> :
+              ''
+          }
+
         </div>
       </div>
     );
   }
 
+  /**
+   *  функция присвоения true или false для htmlAtribut (переменной) SroreAddress
+   */
+  public opClStoreAddress() {
+    if (this.SroreAddress !== true) {
+      this.SroreAddress = true;
+    } else {
+      this.SroreAddress = false;
+    }
+  }
+
+  /**
+   * Вызов модального окна формы
+   */
+  public closeStoreAddress() {
+    if (this.SroreAddress === true) {
+      this.SroreAddress = false;
+    }
+  }
+
+}
+
+/*
+* компонентная функция для вывода блоков
+ */
+const SroreAddress = (props) => {
+  return props.arr.map((item) => {
+    return (
+      <div>
+        <hr/>
+        <div class="store-list-wrapper">
+          <div class="store-list">
+            <div class="store-address-and-work-time">
+                <span class="store-address">
+                  {item.storeAddress}
+                </span>
+              <span class="store-work-time">
+                  {item.storeWorkTime}
+                </span>
+            </div>
+            <button class="store-secet-btn">
+              {item.btnText}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  })
+}
+
+/**
+ * компонентная функция для вывода флага и кода выбранной страны
+ **/
+const CityForSelected = (props) => {
+  return props.arr.map((item) => {
+    return (
+      <div class="city-for-selected">
+        {item.city}
+      </div>
+    );
+  })
 }
--- src/utils/mock-b.ts
+++ src/utils/mock-b.ts
@@ -1,4 +1,3 @@
-
 export const header = {
   topBar: {
     id: 1,
@@ -30,11 +29,220 @@ export const header = {
         city: "Город:",
         selectedCity: "Москва",
         selectIcon: `<i class="fas fa-angle-down"></i>`,
+        cityForSelected: [
+          {
+            id: 1,
+            city: 'Москва'
+          },
+          {
+            id: 2,
+            city: 'Казань'
+          },
+          {
+            id: 3,
+            city: 'Екатеринбург'
+          },
+          {
+            id: 4,
+            city: 'Нижний Новгород'
+          },
+          {
+            id: 5,
+            city: 'Ростов-на-Дону'
+          },
+          {
+            id: 6,
+            city: 'Уфа'
+          },
+          {
+            id: 7,
+            city: 'Краснодар'
+          },
+          {
+            id: 8,
+            city: 'Самара'
+          },
+          {
+            id: 9,
+            city: 'Воронеж'
+          },
+          {
+            id: 10,
+            city: 'Волгоград'
+          },
+          {
+            id: 11,
+            city: 'Омск'
+          },
+          {
+            id: 12,
+            city: 'Челябинск'
+          },
+          {
+            id: 13,
+            city: 'Красноярск'
+          },
+          {
+            id: 14,
+            city: 'Московская Область'
+          },
+          {
+            id: 15,
+            city: 'Пермь'
+          },
+          {
+            id: 16,
+            city: 'Тюмень'
+          },
+          {
+            id: 17,
+            city: 'Одинцово'
+          },
+          {
+            id: 18,
+            city: 'Балашиха'
+          },
+          {
+            id: 19,
+            city: 'Ногинск'
+          },
+          {
+            id: 20,
+            city: 'Подольск'
+          },
+          {
+            id: 21,
+            city: 'Иркутск'
+          },
+          {
+            id: 22,
+            city: 'Калининград'
+          },
+          {
+            id: 23,
+            city: 'Ярославль'
+          },
+          {
+            id: 24,
+            city: 'Владимир'
+          },
+          {
+            id: 25,
+            city: 'Барнаул'
+          },
+          {
+            id: 26,
+            city: 'Тула'
+          },
+          {
+            id: 27,
+            city: 'Курск'
+          },
+          {
+            id: 28,
+            city: 'Кемерово'
+          },
+
+          {
+            id: 29,
+            city: 'Томск'
+          },
+          {
+            id: 30,
+            city: 'Иваново'
+          },
+          {
+            id: 31,
+            city: 'Саратов'
+          },
+          {
+            id: 32,
+            city: 'Новокузнецк'
+          },
+          {
+            id: 33,
+            city: 'Астрахань'
+          },
+          {
+            id: 34,
+            city: 'Ставрополь'
+          },
+          {
+            id: 35,
+            city: 'Ижевск'
+          },
+          {
+            id: 36,
+            city: 'Орел'
+          },
+          {
+            id: 37,
+            city: 'Смоленск'
+          },
+          {
+            id: 38,
+            city: 'Чебоксары'
+          },
+          {
+            id: 39,
+            city: 'Брянск'
+          },
+        ],
         logoImg:
           "https://sbermarket.ru/spree/retailer/icons/234559/original/1.png?1580316602",
         storeName: "Metro",
         numberOfStores: "8 магазинов",
         selectedStoreIcon: `<i class="fas fa-angle-down"></i>`,
+        storeAddress: [
+          {
+            id: 1,
+            storeAddress: 'Москва, Ленинградское шоссе, 71Г',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 2,
+            storeAddress: 'Москва, Маршала Прошлякова, 14',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 3,
+            storeAddress: 'Москва, р-н северного Измайлово, 104-й км. МКАД, 6',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 4,
+            storeAddress: 'Москва, Шоссейная, 2Б',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 5,
+            storeAddress: 'Москва, Дмитровское ш, 165Б',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 6,
+            storeAddress: 'д. Апаринки, МО, п. Совхоза им. Ленина, 8',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 7,
+            storeAddress: 'Москва, Рябиновая, 59',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+          {
+            id: 8,
+            storeAddress: 'Москва, Боровское ш, 10А',
+            storeWorkTime: 'Самовывоз ежедневно с 10:00 до 22:00',
+            btnText: 'Выбрать',
+          },
+        ]
       },
     },
   },
