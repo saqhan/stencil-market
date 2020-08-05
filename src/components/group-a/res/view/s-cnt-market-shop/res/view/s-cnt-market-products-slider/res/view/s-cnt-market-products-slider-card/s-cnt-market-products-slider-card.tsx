7f13d86377b0879c34f7c69d3315ebffb1b505b9
freeside <almalib@bk.ru>
2020-08-05 12:03:03 +0300

    last

--- src/components/group-a/res/view/s-cnt-market-shop/res/view/s-cnt-market-products-slider/res/view/s-cnt-market-products-slider-card/s-cnt-market-products-slider-card.tsx
+++ src/components/group-a/res/view/s-cnt-market-shop/res/view/s-cnt-market-products-slider/res/view/s-cnt-market-products-slider-card/s-cnt-market-products-slider-card.tsx
@@ -10,7 +10,25 @@ export class SCntMarketProductsSliderCard implements ComponentInterface {
 
   render() {
     return (
-      <a href=""></a>
+      <a class="products-card">
+        <div class="products-card-item">
+          <div class="products-card-img">
+
+          </div>
+          <div class="products-card-desc">
+            <div class="products-price">
+              <span class="prise-dis">
+                <span></span>
+              </span>
+              <span class="price-old">
+                <span></span>
+              </span>
+            </div>
+            <p class="products-name"></p>
+            <div class="products-bottom-info"></div>
+          </div>
+        </div>
+      </a>
     );
   }
 
