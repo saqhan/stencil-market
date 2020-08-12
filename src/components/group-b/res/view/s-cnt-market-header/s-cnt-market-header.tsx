10e4d1b3f57f89cff512611653b98d54ab545b22
saqhan <saqhan1@mail.ru>
2020-08-12 17:51:49 +0300

    add sticky

--- src/components/group-b/res/view/s-cnt-market-header/s-cnt-market-header.tsx
+++ src/components/group-b/res/view/s-cnt-market-header/s-cnt-market-header.tsx
@@ -1,4 +1,4 @@
-import {Component, ComponentInterface, h, Prop} from "@stencil/core";
+import { Component, ComponentInterface, h, Prop, State } from "@stencil/core";
 
 @Component({
   tag: "s-cnt-market-header",
@@ -10,21 +10,52 @@ export class SCntMarketHeader implements ComponentInterface {
   /**
    * Массив данных для хедера
    * */
-  @Prop()categories: any;
+  @Prop() categories: any;
 
+  @State() stickyTopTag: HTMLElement;
+
+  componentDidLoad() {
+    window.onscroll = () => {
+      this.stickyTop();
+    };
+  }
   render() {
     return (
       <div class="header-wrapper">
         <div class="container-fluid">
           <div class="row">
             <div class="col-12">
-              <s-cnt-market-top-bar topBar={this.categories.topBar}></s-cnt-market-top-bar>
-              <s-cnt-market-header-body body={this.categories.body} ></s-cnt-market-header-body>
-              <s-cnt-market-header-nav navBar={this.categories.navBar} selectShops={this.categories.selectShops} leftMenuCatalogArr={this.categories.leftMenuCatalog} ></s-cnt-market-header-nav>
+              <div
+                class="header-wrapper-child"
+                ref={(el) => (this.stickyTopTag = el)}
+              >
+                <s-cnt-market-top-bar
+                  topBar={this.categories.topBar}
+                ></s-cnt-market-top-bar>
+                <s-cnt-market-header-body
+                  body={this.categories.body}
+                ></s-cnt-market-header-body>
+                <s-cnt-market-header-nav
+                  navBar={this.categories.navBar}
+                  selectShops={this.categories.selectShops}
+                  leftMenuCatalogArr={this.categories.leftMenuCatalog}
+                ></s-cnt-market-header-nav>
+              </div>
             </div>
           </div>
         </div>
       </div>
     );
+  }
+  /**
+   * Метод для смены внешнего вида главного меню при скролле
+   */
+  public stickyTop() {
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 90) {
+      this.stickyTopTag.classList.add("sticky");
+    } else {
+      this.stickyTopTag.classList.remove("sticky");
+    }
   }
 }
