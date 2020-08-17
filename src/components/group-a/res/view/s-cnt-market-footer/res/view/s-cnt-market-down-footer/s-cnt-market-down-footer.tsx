bf07be61778c2ddffae91030534d7a6208dd958f
saqhan <saqhan1@mail.ru>
2020-08-17 20:52:20 +0300

    add css

--- src/components/group-a/res/view/s-cnt-market-footer/res/view/s-cnt-market-down-footer/s-cnt-market-down-footer.tsx
+++ src/components/group-a/res/view/s-cnt-market-footer/res/view/s-cnt-market-down-footer/s-cnt-market-down-footer.tsx
@@ -18,30 +18,42 @@ export class SCntMarketDownFooter implements ComponentInterface {
       <div class="downFooter">
         <div class="container">
           <div class="row">
-            <div class="col-12 col-sm-6 col-lg-3"><div class="copy ">
-              <div class="copyright">{this.forDownFooter.copyright}</div>
-              <div class="company">{this.forDownFooter.company}</div>
-            </div></div>
-            <div class="col-12 col-sm-6 col-lg-3"><div class="icons">
-              <div class="facebook">
-                <i class="fab fa-facebook-f" />
+            <div class="col-12 col-lg-6">
+              <div class="col-lg-6">
+                <div class="copy ">
+                  <div class="copyright">{this.forDownFooter.copyright}</div>
+                  <div class="company">{this.forDownFooter.company}</div>
+                </div>
               </div>
-              <div class="vk">
-                <i class="fab fa-vk" />
+              <div class="col-lg-6">
+                <div class="icons">
+                  <div class="facebook">
+                    <i class="fab fa-facebook-f" />
+                  </div>
+                  <div class="vk">
+                    <i class="fab fa-vk" />
+                  </div>
+                  <div class="instagram">
+                    <i class="fab fa-instagram" />
+                  </div>
+                </div>
               </div>
-              <div class="instagram">
-                <i class="fab fa-instagram" />
+            </div>
+            <div class="col-12 col-lg-6">
+              <div class="col-lg-6">
+                <div class="download">{this.forDownFooter.download}</div>
               </div>
-            </div></div>
-            <div class="col-12 col-sm-6 col-lg-3"><div class="download">{this.forDownFooter.download}</div></div>
-            <div class="col-12 col-sm-6 col-lg-3"><div class="iconsApp">
-              <div class="apple">
-                <i class="fab fa-apple" />
+              <div class="col-lg-6">
+                <div class="iconsApp">
+                  <div class="apple">
+                    <i class="fab fa-apple" />
+                  </div>
+                  <div class="google">
+                    <i class="fab fa-google-play" />
+                  </div>
+                </div>
               </div>
-              <div class="google">
-                <i class="fab fa-google-play" />
-              </div>
-            </div></div>
+            </div>
           </div>
         </div>
       </div>
