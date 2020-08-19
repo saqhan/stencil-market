97219b362490fe84039dc99c7816051737b8b671
freeside <almalib@bk.ru>
2020-08-19 11:10:23 +0300

    last

--- src/utils/mock-b.ts
+++ src/utils/mock-b.ts
@@ -50,8 +50,8 @@ export const header = {
         },
       ],
       login: {
-<<<<<<< HEAD
-id: 1,
+
+        id: 1,
         rememberMe: 'Запомнить меня',
         logIn: 'Войти',
         remindPass: 'Напомнить пароль',
@@ -107,17 +107,16 @@ id: 1,
           },
         ],
       },
+      // reg: {
+      //   id: 1,
+      //   rememberMe: 'Запомнить меня',
+      //   logIn: 'Войти',
+      //   remindPass: 'Напомнить пароль',
+      //   logInToBank: 'Вход через Сбербанк ID',
+      //   orLogInSocial: 'или авторизуйтесь через'
+      // },
       reg: {
-        id: 1,
-=======
-        rememberMe: 'Запомнить меня',
-        logIn: 'Войти',
-        remindPass: 'Напомнить пароль',
-        logInToBank: 'Вход через Сбербанк ID',
-        orLogInSocial: 'или авторизуйтесь через'
-      },
-      reg: {
->>>>>>> b34cf2e6f2a4c5bed1b5bde096121cfc3f1c50c2
+
         checkForBis: 'Хочу заказывать для бизнеса',
         checkForAdvertisingText: 'Согласен',
         checkForAdvertisingLink: 'на рекламную рассылку',
@@ -517,10 +516,7 @@ id: 1,
               // },
             ]
           },
-<<<<<<< HEAD
-=======
 
->>>>>>> b34cf2e6f2a4c5bed1b5bde096121cfc3f1c50c2
           {
             id: 29,
             city: 'Томск',
