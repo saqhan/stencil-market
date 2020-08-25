d85b90054abe6c827ea2351fa9a75579f001e29e
Wokh-Dada <a.gezikhanov@mail.ru>
2020-08-25 09:44:45 +0300

    added this to the mock

--- src/index.ts
+++ src/index.ts
@@ -172,6 +172,7 @@ export interface MarketRecentlyWatchedProductsInterface {
   weight: string,
   oldPrice?: string,
   disPrice?: string
+  productData?: MarketProductDataItemsInterface;
 }
 
 export interface MarketProductModalInterface {
